51ed615c79d2fff4da9ed1943f5b04dd2957f3d0
Raihan <17radf@gmail.com>
2022-09-27 03:33:44 +0700

    update: switch to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,37 +4,52 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void testNgab() {
-    print("lmao");
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = [
+    "Kamu mau gak jadi pacarku?",
+    "Siapa presiden nomor satu?",
+    "Kuliah penting atau tidak?",
+  ];
+  void answerQuestion() {
+    setState(() {
+      if(questionIndex < questions.length - 1) questionIndex += 1;
+    });
+    print(questionIndex);
+    print(questions.length);
   }
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("ahihaiahi"),
-        ),
+          title: Text("ahihaiahi"),),
         body: Container(
           padding: EdgeInsets.all(30),
           child: Column(children: [
-            Text("Anggap aja ini something"),
+            Text(questions[questionIndex]),
             ElevatedButton(
-                onPressed: testNgab,
+                onPressed: answerQuestion,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [Icon(Icons.question_answer), Text("Answer one")],
                 )),
             ElevatedButton(
-                onPressed: testNgab,
+                onPressed: answerQuestion,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [Icon(Icons.question_answer), Text("Answer two")],
                 )),
             ElevatedButton(
-                onPressed: testNgab,
+                onPressed: answerQuestion,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [Icon(Icons.question_answer), Text("Answer three")],
